0d15476fc5096763326260a6777a9f26915c8e8f
Ashley <ahappyday4all@gmail.com>
2021-04-29 22:25:03 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
+
+app.listen(port, () => {
+  console.log(`App is running on localhost:${port}`)
+})
