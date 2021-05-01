04f435ecdf0a2444a5a46fbdc1866d2fc4c4ada9
Ashley <ahappyday4all@gmail.com>
2021-05-01 13:44:56 +0800

    feat: layout

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+/* Setting view engine */
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+/* Setting static files */
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.render('index')
 })
 
 app.listen(port, () => {
